ad5423894dd81412232ba1aa52f7697ec62914cd
mkg20001 <mkg20001@gmail.com>
2017-05-02 17:04:28 +0200

    Add ci_mode to toggle download progress

--- buildozer/__init__.py
+++ buildozer/__init__.py
@@ -674,8 +674,9 @@ class Buildozer(object):
             else:
                 progression = '{0:.2f}%'.format(
                         index * blksize * 100. / float(size))
-            stdout.write('- Download {}\r'.format(progression))
-            stdout.flush()
+            if "CI_MODE" not in environ:
+                stdout.write('- Download {}\r'.format(progression))
+                stdout.flush()
 
         url = url + filename
         if cwd:
